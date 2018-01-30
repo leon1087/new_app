ca829d001c2e97977103cd82aa4ea4a271a2c7d3
Leon Wang <wangjq10@gmail.com>
2018-01-30 07:36:54 +0000

    add blog views

---
+++ blog/form.py
@@ -0,0 +1,2 @@
+from flask_WTF import Form
+from wtforms import validators, StringField, PasswordField
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,24 @@
 from new_app import app
+from flask import render_template, url_for, redirect
+from author.models import Author
+from blog.models import Blog
+from blog.form import SetupForm
+from new_app import db
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World!"
+    
+@app.route('/admin')
+def admin():
+    blogs = Blog.query.count()
+    if blogs == 0:
+        return redirect('setup')
+    return render_template('blog/admin.html')
+    
+@app.route('/setup', method=('GET', 'POST'))
+def setup():
+    form = SetupForm()
+    return render_template('blog/setup.html', form=form)
+    
