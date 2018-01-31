cb762aafa2585576f5339dc3ef6f85efb2f69088
Leon Wang <wangjq10@gmail.com>
2018-01-31 07:37:45 +0000

    blog views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from new_app import app
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, flash
 from author.models import Author
 from blog.models import Blog
 from blog.form import SetupForm
@@ -14,11 +14,39 @@ def index():
 def admin():
     blogs = Blog.query.count()
     if blogs == 0:
-        return redirect('setup')
+        return redirect(url_for('setup'))
     return render_template('blog/admin.html')
     
-@app.route('/setup', method=('GET', 'POST'))
+@app.route('/setup')
 def setup():
     form = SetupForm()
+    error = ""
+    if form.validate_on_submit():
+        author = Author(
+            form.fullname.data,
+            form.email.data,
+            form.username.data,
+            form.password.data,
+            True
+            )
+        db.session.add(author)
+        db.session.flush()
+        if author.id:
+            blog = Blog(
+                form.name.data,
+                author.id
+                )
+            db.session.add(blog)
+            db.session.flush()
+        else:
+            db.session.rollback()
+            error = "Error creating user."
+        if author.id and blog.id:
+            db.session.commit()
+            flash("Blog Created")
+            return redirect(url_for("admin"))
+        else:
+            db.session.rollback()
+            error = "Error creating blog"
     return render_template('blog/setup.html', form=form)
     
